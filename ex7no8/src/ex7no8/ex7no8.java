d174e88187b1654eb1d94a569d79d0a85d9f62d8
정현우 <80958519+TheRei3@users.noreply.github.com>
2021-08-06 08:16:31 +0900

    ex7no8

---
+++ ex7no8/src/ex7no8/ex7no8.java
@@ -0,0 +1,37 @@
+package ex7no8;
+
+public class ex7no8 {  //  334p
+	public static void main(String[] args) {
+		Point3D p3 = new Point3D(1,2,3);
+		System.out.println("p3.x = " + p3.x);
+		System.out.println("p3.y = " + p3.y);
+		System.out.println("p3.z = " + p3.z);
+	}
+
+}
+
+class Point {
+	int x = 10;
+	int y = 20;
+	
+	Point(int x, int y){
+		
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Point3D extends Point {
+	int z = 30;
+	
+	Point3D() {
+		this(100, 200, 300);
+	}
+	
+	
+	Point3D(int x, int y, int z){
+		super(x,y);	
+		this.z = z;
+	}
+
+}
